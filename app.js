cc15744d90907caf8469bd03dc23a07f1b0971d1
nbg <nicholasnbg@gmail.com>
2017-09-19 13:33:14 +1000

    changes to index mobile nav

--- app.js
+++ app.js
@@ -11,24 +11,42 @@ $(document).ready(function(){
 
   $(document).scroll(function(){
     if($('body').hasClass('index')){
-      console.log('is index');
       var y = $(document).scrollTop();
       if(y>5){
         $('header').css('background-color','white');
         $('.navbar').addClass('scrolled');
       }
       else {
-        $('header').css('background-color','rgba(0 , 0, 0, .0');
-        $('.navbar').removeClass('scrolled');
+        if(widthW>550){
+          $('header').css('background-color','rgba(0 , 0, 0, .0');
+          $('.navbar').removeClass('scrolled');
+        }
     }
     }
-
 
 /*Changes opacity of hero image when scrolled*/
     if(y>0 && y<heightW/2){
       var scrollPerc = (y/(heightW/2))*.5;
       var backgroundAlpha = 0.35*(1-scrollPerc);
       $('.hero').css('box-shadow','inset 0 0 0 1000px rgba(0,0,0,'+backgroundAlpha+')');
+    }
+  })
+
+  /*Mobile Nav functions*/
+  $('.menuToggle').click(function(){
+    if(!($('.menuToggle').hasClass('open'))){
+      console.log('open menu');
+      $('header').css('background-color','white');
+      $('header').css('height','auto');
+      $('.navbar').addClass('scrolled');
+      $('#mobile-menu').show('fast')
+    } else {
+      var y = $(document).scrollTop();
+      if(y<=5){
+        $('header').css('background-color','rgba(0 , 0, 0, .0');
+        $('.navbar').removeClass('scrolled');
+      }
+      $('#mobile-menu').hide('fast')
     }
   })
 
